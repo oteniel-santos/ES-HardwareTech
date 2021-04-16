23eaa3aea941b92094c75872c959be15951e289a
BrunuNC <brununc@gmail.com>
2021-04-16 16:56:33 -0400

    adicionando template e estilos

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
+const { static } = require('express');
 const express = require('express');
 const app = express();
+
+app.get('/', viewIndex); 
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+function viewIndex(req, res) {
+  res.render('index');
+};
 
 app.listen(3000);
