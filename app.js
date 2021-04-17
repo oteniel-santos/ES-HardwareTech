df55a5fe1a56a1dfd90778220412acdfe015386e
BrunuNC <brununc@gmail.com>
2021-04-16 20:36:30 -0400

    adicionado teste rota lista-produtos

--- app.js
+++ app.js
@@ -10,4 +10,32 @@ function viewIndex(req, res) {
   res.render('index');
 };
 
-app.listen(3000);
+app.get('/produtos', (req, res) => {
+  res.render('listar-produtos', produtos);
+
+  //console.log(produtos);
+  /*let html = `<h1>Lista Vinda do Servidor</h1>
+              <ul>`
+  for (const prod of produtos) {
+    html += `
+    <li>
+      Codigo:  ${prod.codigo},
+      Produto: ${prod.produto},
+      Preço:   ${prod.preco}
+    </li>`
+  }              
+  html +=`</ul> `;
+  res.send(html);*/
+
+  //res.sendFile(__dirname + '/public/listar-produtos.html');
+});
+
+app.listen(3000, () => {console.log('Rodando...')});
+
+const produtos = [
+  { codigo: 1, desc: 'Processador', preco: 120 },
+  { codigo: 2, desc: 'Hard Disk',   preco: 50  },
+  { codigo: 3, desc: 'Memória RAM', preco: 100 },
+  { codigo: 4, desc: 'Pen Drive',   preco: 23  },
+  { codigo: 5, desc: 'Impressora',  preco: 450 }
+];
