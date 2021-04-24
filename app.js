875a6aae05a5d105ded4744c9df458ffa7278b92
BrunuNC <brununc@gmail.com>
2021-04-24 18:16:27 -0400

    adicionado cadastro e lista de produto

--- app.js
+++ app.js
@@ -1,24 +1,53 @@
 const { static } = require('express');
 const express = require('express');
+const path = require('path');
 const app = express();
 
+app.use(express.urlencoded({extended: true}));
+
+
 app.get('/', viewIndex); 
-app.use(express.static('public'));
+
+const dirPublic = path.join(__dirname, '/public');
+app.use(express.static(dirPublic));
 app.set('view engine', 'ejs');
 
 function viewIndex(req, res) {
   res.render('index');
 };
 
-app.get('/produtos', (req, res) => {
-  res.render('produtos', {produtos});
+app.get('/listaprodutos', (req, res) => {
+  res.render('listar-produtos', {produtos});
 });
 
 app.get('/usuarios', (req, res) => {
   res.render('usuarios');
 });
 
+app.get('/produto/novo', (req, res) => {
+  res.render('cadastrar-produto')
+});
+
+app.post('/produto', (req, res) => {
+  const produto = addProduto({
+    descricao: req.body.descricao,
+    preco: req.body.preco
+  })
+  res.redirect('/listaprodutos');
+});
+
 app.listen(3000, () => {console.log('Rodando...')});
+
+const novoId = {
+  _id: 6,
+  get id(){return this._id++}
+}
+
+function addProduto(produto){
+  if (!produto.id) produto.codigo = novoId.id
+  produtos.push(produto);
+  return produto  
+}
 
 const produtos = [
   { codigo: 1, desc: 'Processador', preco: 120 },
